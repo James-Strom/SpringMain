78959a067f07f9ebee70a2ccf5e524b87d7dae4e
james-strom <james.stromnes@gmail.com>
2022-02-04 11:35:32 +0200

    Full-Stack Section 5 complete

---
+++ Project1_FullStack/todo-app-app/src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ErrorComponent() {
+    return <div>An Error Occurred. I don't know what to do! Contact support at fghg-fhghf-dfgt</div>
+}
+
+export default ErrorComponent
